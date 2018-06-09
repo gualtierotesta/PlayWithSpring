47aeae8398ac51b099aa6df29695f5af95186bf8
Gualtiero Testa <gualtiero.testa@gmail.com>
2018-06-09 17:06:31 +0200

    Completed logging1 project

--- logging1/src/main/java/it/gualtierotesta/playwithspring/logging1/Logging1Application.java
+++ logging1/src/main/java/it/gualtierotesta/playwithspring/logging1/Logging1Application.java
@@ -2,19 +2,23 @@ package it.gualtierotesta.playwithspring.logging1;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Logging1Application {
+public class Logging1Application implements CommandLineRunner {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Logging1Application.class);
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
+        SpringApplication.run(Logging1Application.class, args);
+    }
 
+    @Override
+    public void run(final String... args) throws Exception {
         LOGGER.info("info: **** Application start");
         LOGGER.debug("debug: **** Application start");
         LOGGER.trace("trace: **** Application start");
-        SpringApplication.run(Logging1Application.class, args);
     }
 }
